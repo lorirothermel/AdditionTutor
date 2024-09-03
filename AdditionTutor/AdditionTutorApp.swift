fff499a621c97e0c275c84897b5394cbe60c629f
lorirothermel <lori@rothermel1.com>
2024-09-03 15:56:28 -0400

    Initial Commit

---
+++ AdditionTutor/AdditionTutorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdditionTutorApp.swift
+//  AdditionTutor
+//
+//  Created by Lori Rothermel on 9/3/24.
+//
+
+import SwiftUI
+
+@main
+struct AdditionTutorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
